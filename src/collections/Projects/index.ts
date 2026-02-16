2e095dc4887f19d0629b5deb88d80f59b9f10171
Aayurt <aayurtshrestha@gmail.com>
2026-02-16 15:37:55 +0545

    Changes

--- src/collections/Projects/index.ts
+++ src/collections/Projects/index.ts
@@ -1,5 +1,6 @@
 import type { CollectionConfig } from 'payload'
 import { updateAndDeleteAccess } from '../Tenants/access/updateAndDelete'
+import { superAdminOrTenantAdminAccess } from '@/access/superAdminOrTenantAdmin'
 
 export const Projects: CollectionConfig = {
   slug: 'projects',
@@ -9,9 +10,9 @@ export const Projects: CollectionConfig = {
   },
   access: {
     create: () => true,
-    delete: updateAndDeleteAccess,
+    delete: superAdminOrTenantAdminAccess,
     read: () => true,
-    update: updateAndDeleteAccess,
+    update: superAdminOrTenantAdminAccess,
   },
   fields: [
     {
@@ -55,4 +56,38 @@ export const Projects: CollectionConfig = {
       hasMany: true,
     },
   ],
+  endpoints: [
+    {
+      path: '/by-slug/:slug',
+      method: 'get',
+
+      handler: async (req) => {
+        const slug = req.routeParams?.slug as string
+        const getTenant = await req.payload.find({
+          collection: 'tenants',
+          where: {
+            slug: {
+              equals: slug,
+            },
+          },
+          limit: 1,
+        })
+        if (getTenant.docs.length === 0) {
+          return Response.json(
+            { message: 'Tenant not found' },
+            { status: 404 }
+          )
+        }
+        const projects = await req.payload.find({
+          collection: 'projects',
+          where: {
+            tenant: {
+              equals: getTenant.docs[0]?.id,
+            },
+          },
+        })
+        return Response.json(projects.docs, { status: 200 })
+      },
+    },
+  ]
 }
